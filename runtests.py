f39af5e5f5483a0e14cabb1f2f556ba9368bdaf5
Kenneth Reitz <me@kennethreitz.org>
2018-03-16 18:00:12 -0400

    runtests
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- runtests.py
+++ runtests.py
@@ -15,7 +15,6 @@
 import os.path
 import sys
 import subprocess
-import shutil
 
 def run(cmd):
     print(cmd)
@@ -25,19 +24,14 @@ venv = "test-venv-py{}{}".format(sys.version_info[0], sys.version_info[1])
 
 if not os.path.exists(venv):
     print("-- Creating venv in {} --".format(venv))
-    run([sys.executable, "-m", "virtualenv", venv])
+    run([sys.executable, "-m", "venv", venv])
 
-run([venv + "/bin/pip", "install", "-r", "dev-requirements.txt"])
+run([venv + "/bin/python", "-m", "pip", "install", "-r", "dev-requirements.txt"])
 # XX get rid of this:
-run([venv + "/bin/pip", "install", "trio", "twisted[tls]"])
+run([venv + "/bin/python", "-m", "pip", "install", "trio", "twisted[tls]"])
 
 print("-- Rebuilding urllib3/_sync in source tree --")
-run([sys.executable, "setup.py", "build"])
-try:
-    shutil.rmtree("urllib3/_sync")
-except FileNotFoundError:
-    pass
-shutil.copytree("build/lib/urllib3/_sync", "urllib3/_sync")
+run([sys.executable, "setup.py", "compile"])
 
 print("-- Running tests --")
 run([venv + "/bin/python", "-m", "pytest"] + list(sys.argv)[1:])
